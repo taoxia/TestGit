ce088e94ad9741d69cef4eb621a5c467a89a4779
Taotao <929408131@qq.com>
2018-06-02 22:17:08 +0800

    add MainActivity

---
+++ app/src/main/java/com/tao/testgit/MainActivity.java
@@ -0,0 +1,17 @@
+package com.tao.testgit;
+
+import android.provider.Settings;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        System.out.print("1223213");
+    }
+}
